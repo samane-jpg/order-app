f275d53eecf9d21273e2ff766227ec9083656a31
samane <samanemahdavian@gmail.com>
2023-01-03 00:34:08 +0330

    Add file and folders

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+
+const Header = (props) => {
+     return ( 
+          <Fragment>
+               <header>
+                    <h1>Foodies</h1>
+                    <button>Cart</button>
+               </header>
+               <div>
+                    <img/>
+               </div>
+          </Fragment>
+          
+      );
+}
+ 
+export default Header;
