ff1d98800012a9ff93e4e933fb7ccbc6c1194aad
samane <samanemahdavian@gmail.com>
2023-01-03 00:55:10 +0330

    Add style to header + picture

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,16 @@
 import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
      return ( 
           <Fragment>
                <header>
-                    <h1>Foodies</h1>
+                    <h1 className={classes.header}>Foodies</h1>
                     <button>Cart</button>
                </header>
-               <div>
-                    <img/>
+               <div className={classes["main-image"]}>
+                    <img src={mealsImage} alt="It is a table full of foods"/>
                </div>
           </Fragment>
           
