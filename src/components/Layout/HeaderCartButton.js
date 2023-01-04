0f8fbeb7cc0d22608256291503d511a1dad5a1d6
samane <samanemahdavian@gmail.com>
2023-01-04 18:38:28 +0330

    Add style to the header

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,7 @@
 import CartIcon from "./../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-const HeaderCartButton = (props) => {
+
+const HeaderCartButton = () => {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
