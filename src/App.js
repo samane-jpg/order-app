e2b1933b66aac7c5e09bc8c8f8e3f03c86a58436
samane <samanemahdavian@gmail.com>
2023-01-14 17:40:32 +0330

    Add basic cartProvider and cartContext

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Layout/Header";
 import { Fragment, useState } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,14 +16,14 @@ function App() {
     setCartIsShown(false);
   }
   return (
-    <Fragment className="App">
+    <CartProvider className="App">
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       
       <Header onShowCart={showCatHandler}/>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
