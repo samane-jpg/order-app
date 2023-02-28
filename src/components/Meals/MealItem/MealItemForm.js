0c80abe6785b2e42ac16aedcd714b5f533b898c3
samane <samanemahdavian@gmail.com>
2023-03-01 01:01:09 +0330

    check the validity of number of food

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,26 @@
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
-import { useRef
- } from "react";
+import { useRef } from "react";
 const MealItemForm = (props) => {
   const amountInputRef = useRef();
 
-  const submitHandler = (event) =>{
+  const submitHandler = (event) => {
     event.preventDefault();
-  }
-  
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) return;
+  };
+
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        ref = {amountInputRef}
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
