7fbc7e47ca6c7471a28ed37858950b465194b0e1
samane <samanemahdavian@gmail.com>
2023-03-02 11:08:46 +0330

    Showing the amount of added meal to cart badge

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,11 @@
-import classes from "./Input.module.css"
-import React
- from "react";
+import classes from "./Input.module.css";
+import React from "react";
+
 const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input ref={ref} {...props.input}/>
+      <input ref={ref} {...props.input} />
     </div>
   );
 });
