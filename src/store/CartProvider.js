40a4a12602e26bfba4f9336da2973329a9a08fe0
samane <samanemahdavian@gmail.com>
2023-03-03 20:15:11 +0330

    Put all the same food in cart list under the one item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,14 +8,31 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
-    const updatedItem = state.items.concat(action.item);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
     
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-      items: updatedItem,
+      items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
