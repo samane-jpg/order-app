9d9f739de898ffecd1677585449f07f33ce73fc4
samane <samanemahdavian@gmail.com>
2023-05-02 18:25:25 +0330

    fixing total amount issue when decreasig in cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -41,7 +41,7 @@ const cartReducer = (state, action) => {
       (item) => item.id === action.id
     );
     const existingItem = state.items[existingCartItemIndex];
-    const updatedTotalAmount = state.totalAmount - existingItem.amount;
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
     let updatedItems;
     if (existingItem.amount === 1) {
       updatedItems = state.items.filter((item) => item.id !== action.id);
